cbee0a3de3167735af6bd60aee2d722d29b6bb85
zubayertahmid <zubayertahmid2218@gmail.com>
2024-10-07 12:10:38 +0600

    Update scroll based velocity

--- components/demos/scroll-based-velocity-demo.tsx
+++ components/demos/scroll-based-velocity-demo.tsx
@@ -12,16 +12,16 @@
 import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 
 export function ScrollBasedVelocityDemo() {
-  return (
-    <VelocityScroll
-      default_velocity={5}
-      className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-blue-400 drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
-    >
-      <img
-        src="/logo/logo.png"
-        alt="Logo"
-        className="w-32 h-auto md:w-48"
-      />
-    </VelocityScroll>
-  );
+  // return (
+  //   // <VelocityScroll
+  //   //   default_velocity={5}
+  //   //   className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-blue-400 drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+  //   // >
+  //   //   <img
+  //   //     src="/logo/logo.png"
+  //   //     alt="Logo"
+  //   //     className="w-32 h-auto md:w-48"
+  //   //   />
+  //   // </VelocityScroll>
+  // );
 }
